8dc92d28688d8a85162d379edda0d7aa9e449e5a
cesar laurino <cesarlaurino@hotmail.com>
2023-02-21 15:51:48 -0300

    arregle un problema en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import ItemDetailContainer from "./components/ItemListContainer";
-import ItemListContainer from "./components/ItemDetailContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
