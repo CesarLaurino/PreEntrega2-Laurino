b4b3001ce6c2f62a2eca21f0b595a6c388bd9a44
cesar laurino <cesarlaurino@hotmail.com>
2023-03-18 13:29:53 -0300

    haciendo cambios generales para poder terminar la pagina

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -19,7 +19,7 @@ const ItemListContainer = () => {
         })
     }, [id]);
 
-
+    console.log(items)
     return (
             <div className="row container-card">
                 <ItemList items = {items}  />
@@ -28,3 +28,38 @@ const ItemListContainer = () => {
 }
 
 export default ItemListContainer;
+
+
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
+// import ItemList from "./ItemList";
+// import Loading from "./Loading";
+
+// const ItemListContainer = () => {
+//     const [items, setItems] = useState([]);
+//     const [loading, setLoading] = useState(true);
+//     const {id} = useParams();
+
+//     useEffect(() => {
+//         const db = getFirestore();
+//         const itemsCollection = collection(db, "items");
+//         const filter = id ? query(itemsCollection, where("categoria", "==", id)) : itemsCollection;
+//         getDocs(filter).then(elements => {
+//             setItems(elements.docs.map(element => ({id:element.id, ...element.data()})));
+//             setLoading(false);
+//         // arrayProductos.forEach(item =>{
+//         //     addDoc(itemsCollection,item);
+//         });
+//         // console.log("se agregaron los Productos!")
+
+//     }, [id]);
+
+//     return (
+//         <div className="container">
+//             {loading ? <Loading /> : <ItemList items={items} />}
+//         </div>
+//     )
+// }
+
+// export default ItemListContainer;
